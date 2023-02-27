f6d52367d6efb26a94f3bf498bbc377664f13e8c
Rohit Dongre <25kishordongre@gmail.com>
2023-02-28 00:13:56 +0530

    Adding Css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import logo from '../../assets/CKlogo.png';
 import './navbar.css';
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='Checkaro__navbar'>
+      <div className='Checkaro__navbar-links'>
+      <div className='Checkaro__navbar-links_logo'>
+        <img src={logo} alt="logo" />
+      </div>
+      <div className='Checkaro__navbar-links_container'></div>
+      <p><a href='#login'>Log In/Sign Up</a></p>
+      <p><a href='#Cart'>Cart</a></p>
+      </div>
+     </div>
   )
 }
 
